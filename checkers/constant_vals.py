96f49065aa212e123c41055c9c1bbf4402ca502c
Adil Biju <adilbiju@gmail.com>
2026-01-27 13:24:00 -0500

    Add board and piece drawing colors

--- checkers/constant_vals.py
+++ checkers/constant_vals.py
@@ -7,6 +7,26 @@ ROWS = 8
 COLS = 8
 SQUARE_SIZE = WIDTH // COLS
 
-RED = (160, 25, 25)
-WHITE = (240, 240, 240)
-BLACK = (0, 0, 0)
+CHECK1 = (79, 79, 79)
+CHECK2 = (30, 30, 30)
+
+RED = (160, 25 ,25)
+WHITE = (240,240,240)
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+GREY = (128,128,128)
+YELLOW = (255, 215, 0)
+
+
+CROWN = crown_points = [
+    (100, 200),  # bottom left
+    (120, 120),  # first peak
+    (150, 200),  # down
+    (180, 120),  # second peak
+    (210, 200),  # down
+    (240, 120),  # third peak
+    (270, 200),  # down
+    (300, 120),  # fourth peak
+    (330, 200),  # down
+]
+
