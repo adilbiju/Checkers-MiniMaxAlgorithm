89e616202f927cb85f50973f7e0c80b4e8e2350c
Adil Biju <adilbiju@gmail.com>
2026-07-06 20:14:00 -0400

    Choose moves with minimax

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -2,6 +2,30 @@ from copy import deepcopy
 
 from checkers.constant_vals import RED, WHITE
 
+def minimax(position, depth, max_player, game):
+    turn = WHITE if max_player else RED
+    if depth == 0 or position.winner(turn) is not None:
+        return position.evaluate(), position
+    
+    if max_player:
+        maxEval = float('-inf')
+        best_move = None
+        for move in get_all_moves(position, WHITE, game):
+            evaluation = minimax(move, depth-1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+        return maxEval, best_move
+    else:
+        minEval = float('inf')
+        best_move = None
+        for move in get_all_moves(position, RED, game):
+            evaluation = minimax(move, depth-1, True, game)[0]
+            minEval = min(minEval, evaluation)
+            if minEval == evaluation:
+                best_move = move
+        return minEval, best_move
+
 def get_all_moves(board, color, game):
     moves = []
 
